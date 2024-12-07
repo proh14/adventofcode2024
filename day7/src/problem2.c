e2850e253f79fd5710ab9bf2bf7adaf10c591432
Hoorad Farrokh <hourrad.f@gmail.com>
2024-12-08 11:21:52 +1300

    Day7: problem2

--- day7/src/problem2.c
+++ day7/src/problem2.c
@@ -1,6 +1,61 @@
+#define _DEFAULT_SOURCE
+
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define INPUT_FILE "../res/input.txt"
+#define MAX_NUMS 25
+#define BUFFSIZE 256
+
+int readLine(char *str, int *nums, unsigned long *cala) {
+  unsigned long cal = 0;
+  sscanf(str, "%lu:", &cal);
+
+  while (*str && *str != ' ')
+    str++;
+
+  char *token;
+  char *saveptr = str;
+
+  int i = 0;
+  while ((token = strtok_r(saveptr, " ", &saveptr)) != NULL) {
+    nums[i++] = atoi(token);
+  }
+  *cala = cal;
+
+  return i;
+}
+int validateCal(int *nums, int idx, int numsize, unsigned long cal,
+                unsigned long res) {
+  if (idx >= numsize)
+    return res == cal;
+
+  char cat[40];
+  snprintf(cat, 40, "%lu%d", res, nums[idx]);
+
+  return validateCal(nums, idx + 1, numsize, cal, nums[idx] + res) ||
+         validateCal(nums, idx + 1, numsize, cal, nums[idx] * res) ||
+         validateCal(nums, idx + 1, numsize, cal, atol(cat));
+}
 
 int main(void) {
-  printf("Hello, world!");
+
+  FILE *fp = fopen(INPUT_FILE, "r");
+  uint64_t ans = 0;
+  char buffer[BUFFSIZE];
+
+  while (fgets(buffer, BUFFSIZE, fp)) {
+    int nums[MAX_NUMS];
+    unsigned long cal = 0;
+    int numlen = readLine(buffer, nums, &cal);
+    if (validateCal(nums, 0, numlen, cal, 0))
+      ans += cal;
+  }
+
+  printf("Answer = %lu :)\n", ans);
+
+  fclose(fp);
   return 0;
 }
