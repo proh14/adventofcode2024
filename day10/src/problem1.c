d06c7eca9806875aafccd9720c3af6453ecbae39
Hoorad Farrokh <hourrad.f@gmail.com>
2024-12-11 21:25:44 +1300

    Day10: problem1 and problem2

--- day10/src/problem1.c
+++ day10/src/problem1.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 #define INPUT_FILE "../res/input.txt"
 
-#define COLS 8
-#define ROWS 8
+#define COLS 45
+#define ROWS 45
+#define NINES 500
 
+struct nine {
+  int row;
+  int col;
+};
+
+struct nine nines[500];
+int nine_size = 0;
 int content[ROWS][COLS];
 
 void readFile(FILE *fp) {
@@ -23,34 +30,37 @@ void readFile(FILE *fp) {
   }
 }
 
+int doesNineExist(int row, int col) {
+  for (int i = 0; i < nine_size; i++) {
+    if (nines[i].col == col && nines[i].row == row) {
+      return 1;
+    }
+  }
+  return 0;
+}
+
 int isTrail(int row, int col, int h) {
 
   if (row < 0 || row >= ROWS || col < 0 || col >= COLS)
     return 0;
   if (content[row][col] != h)
     return 0;
-  if (content[row][col] != h) {
+  if (content[row][col] == 9) {
+    if (!doesNineExist(row, col)) {
+      nines[nine_size].row = row;
+      nines[nine_size++].col = col;
+    }
     return 1;
   }
 
-  int up = isTrail(row + 1, col, h + 1);
-  int down = isTrail(row - 1, col, h + 1);
-  int left = isTrail(row, col + 1, h + 1);
-  int right = isTrail(row, col - 1, h + 1);
+  int up = 0, down = 0, left = 0, right = 0;
+
+  down = isTrail(row + 1, col, h + 1);
+  up = isTrail(row - 1, col, h + 1);
+  right = isTrail(row, col + 1, h + 1);
+  left = isTrail(row, col - 1, h + 1);
 
   return up + down + left + right;
-}
-
-int countTrails(int row, int col) {
-
-  int score = 0;
-
-  score += isTrail(row + 1, col, 1);
-  score += isTrail(row - 1, col, 1);
-  score += isTrail(row, col + 1, 1);
-  score += isTrail(row, col - 1, 1);
-
-  return score;
 }
 
 int main(void) {
@@ -59,15 +69,17 @@ int main(void) {
   readFile(fp);
 
   int ans = 0;
-
   for (int i = 0; i < ROWS; i++) {
     for (int j = 0; j < COLS; j++) {
-      if (content[i][j] == 0)
-        ans += countTrails(i, j);
+      if (content[i][j] == 0) {
+        nine_size = 0;
+        isTrail(i, j, 0);
+        ans += nine_size;
+      }
     }
   }
 
-  printf("Answer = %d :)", ans);
+  printf("Answer = %d :)\n", ans);
 
   fclose(fp);
   return 0;
--- day10/src/problem2.c
+++ day10/src/problem2.c
@@ -1,6 +1,63 @@
 #include <stdio.h>
 
+#define INPUT_FILE "../res/input.txt"
+
+#define COLS 45
+#define ROWS 45
+
+int content[ROWS][COLS];
+
+void readFile(FILE *fp) {
+  int ch = 0;
+  int row = 0;
+  int col = 0;
+
+  while ((ch = getc(fp)) != EOF) {
+    if (ch != '\n') {
+      content[row][col++] = ch - '0';
+      continue;
+    }
+    row++;
+    col = 0;
+  }
+}
+
+int isTrail(int row, int col, int h) {
+
+  if (row < 0 || row >= ROWS || col < 0 || col >= COLS)
+    return 0;
+  if (content[row][col] != h)
+    return 0;
+  if (content[row][col] == 9) {
+    return 1;
+  }
+
+  int up = 0, down = 0, left = 0, right = 0;
+
+  down = isTrail(row + 1, col, h + 1);
+  up = isTrail(row - 1, col, h + 1);
+  right = isTrail(row, col + 1, h + 1);
+  left = isTrail(row, col - 1, h + 1);
+
+  return up + down + left + right;
+}
+
 int main(void) {
-  printf("Hello, world!");
+  FILE *fp = fopen(INPUT_FILE, "r");
+
+  readFile(fp);
+
+  int ans = 0;
+  for (int i = 0; i < ROWS; i++) {
+    for (int j = 0; j < COLS; j++) {
+      if (content[i][j] == 0) {
+        ans += isTrail(i, j, 0);
+      }
+    }
+  }
+
+  printf("Answer = %d :)\n", ans);
+
+  fclose(fp);
   return 0;
 }
